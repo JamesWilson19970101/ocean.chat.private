3014d36d366a4036f2e5dded37b1b19c0e11267e
seconp <15290631595@163.com>
2025-03-04 23:19:34 +0800

    I imitated the official document to add jwt, but it has not been added successfully. I plan to handle the database connection first

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,34 +1,11 @@
-// /home/seconp/ocean.chat.private/src/auth/auth.module.ts
-// TODO： add jwt
 import { Module } from '@nestjs/common';
-import { ConfigModule, ConfigService } from '@nestjs/config';
-import { JwtModule } from '@nestjs/jwt';
-import { PassportModule } from '@nestjs/passport';
-import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
-import { JwtStrategy } from './strategies/jwt.strategy';
-import { GoogleStrategy } from './strategies/google.strategy';
-import { WechatStrategy } from './strategies/wechat.strategy';
-//import {UsersModule} from "../users/users.module"; // Uncomment if needed
+import { AuthController } from './auth.controller';
+import { UsersModule } from '../users/users.module';
 
 @Module({
-  //imports: [UsersModule,
-  imports: [
-    ConfigModule, // Import ConfigModule to access configuration
-    PassportModule.register({ defaultStrategy: 'jwt' }),
-    JwtModule.registerAsync({
-      imports: [ConfigModule],
-      useFactory: async (configService: ConfigService) => ({
-        secret: configService.get<string>('jwt.secret'), // Get the JWT secret from configuration
-        signOptions: {
-          expiresIn: configService.get<string>('jwt.expiresIn'), // Get the JWT expiration time from configuration
-        },
-      }),
-      inject: [ConfigService],
-    }),
-  ],
+  imports: [UsersModule],
+  providers: [AuthService],
   controllers: [AuthController],
-  providers: [AuthService, JwtStrategy, GoogleStrategy, WechatStrategy],
-  exports: [AuthService], // Export AuthService if it's needed elsewhere
 })
 export class AuthModule {}
