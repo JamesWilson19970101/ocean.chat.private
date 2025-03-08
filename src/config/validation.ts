5b823c6e20c4b3eda7e066d436c8966eb1cadc0d
James <happyjames19970101@gmail.com>
2025-03-08 09:03:36 +0800

    The Error object was not originally designed to be called and executed.

--- src/config/validation.ts
+++ src/config/validation.ts
@@ -1,10 +1,12 @@
 import * as Joi from 'joi';
 
 export const validationSchema = Joi.object({
-  DATABASE_URI: Joi.string()
-    .required()
-    .error(new Error('DATABASE_URI is required')),
-  DATABASE_NAME: Joi.string()
-    .required()
-    .error(new Error('DATABASE_NAME is required')),
+  DATABASE_URI: Joi.string().required().messages({
+    'string.empty': 'DATABASE_URI is required',
+    'any.required': 'DATABASE_URI is required',
+  }),
+  DATABASE_NAME: Joi.string().required().messages({
+    'string.empty': 'DATABASE_NAME is required',
+    'any.required': 'DATABASE_NAME is required',
+  }),
 });
