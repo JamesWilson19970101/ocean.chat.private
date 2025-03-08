3e8f96c75012d28fb0aba9b9390a82f9eef5fc01
James <happyjames19970101@gmail.com>
2025-03-08 11:45:26 +0800

    Modify appservice to test whether config is loaded normally.

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,7 +1,29 @@
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class AppService {
+  currentEnv: string;
+  constructor(private readonly configService: ConfigService) {
+    // Access NODE_ENV
+    this.currentEnv =
+      this.configService.get<string>('NODE_ENV') || 'development';
+    console.log(`Current Environment: ${this.currentEnv}`);
+
+    // Check the Environment
+    if (this.currentEnv === 'development') {
+      console.log('You are in the development environment.');
+    } else {
+      console.log('You are in the production or other environment.');
+    }
+
+    // Example of accessing other config variables:
+    const databaseUri = this.configService.get<string>('database.uri');
+    const databaseName = this.configService.get<string>('database.name');
+    console.log(`Database URI: ${databaseUri}`);
+    console.log(`Database Name: ${databaseName}`);
+  }
+
   getHello(): string {
     return 'Hello World!';
   }
