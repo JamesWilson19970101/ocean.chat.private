e0737a293767be377513840d746005b5b23785ba
James <happyjames19970101@gmail.com>
2025-03-14 17:46:57 +0800

    1.use turbo manage monorepo.2.Use ts to convert the json file into a js file, which will be provided to i18next later.

---
+++ packages/i18n/src/generator.ts
@@ -0,0 +1,50 @@
+import fs from 'fs';
+import path from 'path';
+
+const files: string[] = fs.readdirSync('./locales');
+const en: string = fs.readFileSync(`./locales/en.i18n.json`, 'utf8');
+
+const generateJson: (files: string[]) => string = (files) =>
+  files
+    .map((file) => {
+      // Extract the json object and generate the js object later
+      const lang: string = path.basename(file, '.i18n.json');
+      const parsedJson: Record<string, string> = JSON.parse(
+        fs.readFileSync(`./locales/${file}`, 'utf8'),
+      );
+      return `\n"${lang}": ${JSON.stringify(parsedJson, null, 2)}`;
+    })
+    .join(',');
+const contents: string = generateJson(files);
+
+// Simulate esm export
+const esm = `export default {
+	${contents}
+}`;
+
+// By default, en.i18n.json has the most complete translations.
+const keys = Object.keys(JSON.parse(en) as Record<string, string>);
+
+// Simulate ts declarations
+const declarations = `export interface OceanchatI18n {
+	${keys.map((key) => `${JSON.stringify(key)}: string;`).join('\n\t')}
+}
+
+const dict: {
+	[language: string]: OceanchatI18n;
+};
+
+export type OceanchatI18nKeys = keyof OceanchatI18n;
+
+export = dict;
+`;
+
+// write the files
+if (fs.existsSync(`./dist`)) {
+  fs.rmdirSync(`./dist`, { recursive: true });
+}
+
+fs.mkdirSync(`./dist`);
+
+fs.writeFileSync(`./dist/index.js`, esm);
+fs.writeFileSync(`./dist/index.d.ts`, declarations);
