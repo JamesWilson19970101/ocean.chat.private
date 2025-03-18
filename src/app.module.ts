c93e9c9dc943b2222929a6c4f85cd2090c8b5e9c
seconp <15290631595@163.com>
2025-03-18 23:00:43 +0800

    import and set up logger

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,13 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { LoggerModule } from 'nestjs-pino';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { DatabaseModule } from './database/database.module';
 
 @Module({
-  imports: [ConfigModule, DatabaseModule],
+  imports: [ConfigModule, DatabaseModule, LoggerModule.forRoot()],
   controllers: [AppController],
   providers: [AppService],
 })
