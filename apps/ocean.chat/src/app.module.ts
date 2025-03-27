97890086164979d67b73382142361ce8c9b29f11
seconp <15290631595@163.com>
2025-03-27 23:35:10 +0800

    config logger

--- apps/ocean.chat/src/app.module.ts
+++ apps/ocean.chat/src/app.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { IncomingMessage, ServerResponse } from 'http';
 import { LoggerModule } from 'nestjs-pino';
 
 import { AppController } from './app.controller';
@@ -19,7 +20,29 @@ import { DatabaseModule } from './database/database.module';
     }),
     LoggerModule.forRoot({
       pinoHttp: {
+        ...(process.env.NODE_ENV !== 'production'
+          ? {
+              transport: {
+                target: 'pino-pretty',
+                options: {
+                  colorize: true,
+                  singleLine: true,
+                },
+              },
+            }
+          : {}),
         level: process.env.NODE_ENV !== 'production' ? 'debug' : 'info',
+        serializers: {
+          req: (req: IncomingMessage & { id?: number | string }) => {
+            // Add type hint for req
+            return {
+              id: req.id,
+              method: req.method,
+              url: req.url,
+            };
+          },
+          res: (res: ServerResponse) => ({ statusCode: res.statusCode }),
+        },
       },
     }),
     DatabaseModule,
