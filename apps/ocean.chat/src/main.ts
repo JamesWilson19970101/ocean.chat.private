636d6263e3315b1c497acfa54594164da476e55f
James <happyjames19970101@gmail.com>
2025-04-08 09:16:48 +0800

    import swagger

--- apps/ocean.chat/src/main.ts
+++ apps/ocean.chat/src/main.ts
@@ -1,4 +1,5 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { Logger } from 'nestjs-pino';
 
 import { AppModule } from './app.module';
@@ -6,6 +7,17 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { bufferLogs: true });
   app.useLogger(app.get(Logger));
+  if (process.env.NODE_ENV !== 'production') {
+    const options = new DocumentBuilder()
+      .setTitle('Ocean Chat Api')
+      .setDescription('The ocean chat API description')
+      .setVersion('1.0')
+      .addTag('oceanchat')
+      .addBearerAuth()
+      .build();
+    const document = SwaggerModule.createDocument(app, options);
+    SwaggerModule.setup('api', app, document);
+  }
   await app.listen(process.env.OCEAN_CHAT_PORT ?? 3000);
 }
 bootstrap().catch((error) => {
