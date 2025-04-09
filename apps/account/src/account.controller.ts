0a365966704ebdb13882c5e3fb5d63a320c90ef6
James <happyjames19970101@gmail.com>
2025-04-09 18:17:13 +0800

    Use swagger's annotations to document signIn and signUp.

--- apps/account/src/account.controller.ts
+++ apps/account/src/account.controller.ts
@@ -6,24 +6,37 @@ import {
   HttpStatus,
   Post,
 } from '@nestjs/common';
+import {
+  ApiBearerAuth,
+  ApiOperation,
+  ApiResponse,
+  ApiTags,
+} from '@nestjs/swagger';
 
 import { AccountService } from './account.service';
 import { SignInDto, SignUpDto } from './dtos/account.dto';
+
+@ApiBearerAuth()
+@ApiTags('authentication')
 @Controller()
 export class AccountController {
   constructor(private readonly accountService: AccountService) {}
 
+  @ApiOperation({ summary: 'Get hello message' })
+  @ApiResponse({ status: 200, description: 'Hello World.' })
   @Get()
   getHello(): string {
     return this.accountService.getHello();
   }
 
+  @ApiOperation({ summary: 'User login' })
   @HttpCode(HttpStatus.OK)
   @Post('login')
   signIn(@Body() signInDto: SignInDto) {
     return this.accountService.signIn(signInDto.username, signInDto.password);
   }
 
+  @ApiOperation({ summary: 'User signUp' })
   @HttpCode(HttpStatus.CREATED)
   @Post('signup')
   signUp(@Body() signUpDto: SignUpDto) {
--- apps/account/src/dtos/account.dto.ts
+++ apps/account/src/dtos/account.dto.ts
@@ -1,8 +1,12 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsEmail, IsNotEmpty, IsString, MinLength } from 'class-validator';
 export class SignInDto {
+  @ApiProperty({ example: 'james' })
   @IsNotEmpty()
   @IsString()
   readonly username: string;
+
+  @ApiProperty({ example: '12345678' })
   @IsNotEmpty()
   @MinLength(8, {
     // TODO: use i18n module translate message
@@ -12,14 +16,25 @@ export class SignInDto {
   readonly password: string;
 }
 export class SignUpDto {
+  @ApiProperty({
+    required: false,
+  })
   @IsString()
-  readonly name: string;
+  readonly name?: string;
+
+  @ApiProperty()
   @IsNotEmpty()
   @IsString()
   readonly username: string;
+
+  @ApiProperty()
   @IsNotEmpty()
   @IsString()
   readonly password: string;
+
+  @ApiProperty({
+    required: false,
+  })
   @IsEmail()
   @IsString()
   readonly email?: string;
