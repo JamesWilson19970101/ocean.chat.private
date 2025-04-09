b0fe262c24d4366a03baddd793170ced378dffaf
James <happyjames19970101@gmail.com>
2025-04-09 17:10:49 +0800

    config logger & swagger & validation in main.ts

--- apps/account/src/main.ts
+++ apps/account/src/main.ts
@@ -1,9 +1,26 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { Logger } from 'nestjs-pino';
 
 import { AccountModule } from './account.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AccountModule);
+  const app = await NestFactory.create(AccountModule, { bufferLogs: true });
+  app.useLogger(app.get(Logger));
+  app.useGlobalPipes(new ValidationPipe());
+  if (process.env.NODE_ENV !== 'production') {
+    const options = new DocumentBuilder()
+      .setTitle('Ocean Chat Api')
+      .setDescription('The ocean chat API description')
+      .setVersion('1.0')
+      .addTag('oceanchat')
+      .addBearerAuth()
+      .build();
+    const document = SwaggerModule.createDocument(app, options);
+    SwaggerModule.setup('api', app, document);
+  }
+
   await app.listen(process.env.ACCOUNTS_PORT ?? 3001);
 }
 bootstrap().catch(console.error);
