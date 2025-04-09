0ec93b8525569f30966556725ce5198f29b6f97a
James <happyjames19970101@gmail.com>
2025-04-09 17:04:10 +0800

    add login & signup route

--- apps/account/src/account.controller.ts
+++ apps/account/src/account.controller.ts
@@ -1,7 +1,14 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Post,
+} from '@nestjs/common';
 
 import { AccountService } from './account.service';
-
+import { SignInDto, SignUpDto } from './dtos/account.dto';
 @Controller()
 export class AccountController {
   constructor(private readonly accountService: AccountService) {}
@@ -9,5 +16,17 @@ export class AccountController {
   @Get()
   getHello(): string {
     return this.accountService.getHello();
+  }
+
+  @HttpCode(HttpStatus.OK)
+  @Post('login')
+  signIn(@Body() signInDto: SignInDto) {
+    return this.accountService.signIn(signInDto.username, signInDto.password);
+  }
+
+  @HttpCode(HttpStatus.CREATED)
+  @Post('signup')
+  signUp(@Body() signUpDto: SignUpDto) {
+    return this.accountService.signUp(signUpDto);
   }
 }
