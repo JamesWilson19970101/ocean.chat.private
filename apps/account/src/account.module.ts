e8fa1fbac0f755d7ea59814d11b8c4c6b0d8cfe5
James <happyjames19970101@gmail.com>
2025-04-09 17:01:29 +0800

    import I18nModule/LoggerModule/ModelsModule/JwtModule, and config all module.

--- apps/account/src/account.module.ts
+++ apps/account/src/account.module.ts
@@ -1,22 +1,62 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { JwtModule } from '@nestjs/jwt';
+import { I18nModule } from '@ocean.chat/i18n';
+import { ModelsModule } from '@ocean.chat/models';
+import { IncomingMessage, ServerResponse } from 'http';
+import { LoggerModule } from 'nestjs-pino';
 
 import { AccountController } from './account.controller';
 import { AccountService } from './account.service';
 import configuration from './config/configuration';
 import { Env } from './config/env';
 import { validationSchema } from './config/validation';
+import { jwtConstants } from './constants';
 import { DatabaseModule } from './database/database.module';
 
 @Module({
   imports: [
+    I18nModule.forRoot(),
     ConfigModule.forRoot({
       load: [configuration],
       isGlobal: true,
       validationSchema,
       envFilePath: `.env.${process.env.NODE_ENV || Env.Development}`,
     }),
+    LoggerModule.forRoot({
+      pinoHttp: {
+        ...(process.env.NODE_ENV !== 'production'
+          ? {
+              transport: {
+                target: 'pino-pretty',
+                options: {
+                  colorize: true,
+                  singleLine: true,
+                },
+              },
+            }
+          : {}),
+        level: process.env.NODE_ENV !== 'production' ? 'debug' : 'info',
+        serializers: {
+          req: (req: IncomingMessage & { id?: number | string }) => {
+            // Add type hint for req
+            return {
+              id: req.id,
+              method: req.method,
+              url: req.url,
+            };
+          },
+          res: (res: ServerResponse) => ({ statusCode: res.statusCode }),
+        },
+      },
+    }),
     DatabaseModule,
+    ModelsModule,
+    JwtModule.register({
+      global: true,
+      secret: jwtConstants.secret,
+      signOptions: { expiresIn: '600s' },
+    }),
   ],
   controllers: [AccountController],
   providers: [AccountService],
