9f7d189b24754086f3a0f8f8eb054a6dc16ad270
James <happyjames19970101@gmail.com>
2025-04-09 17:13:51 +0800

    add constrains for every field.

--- libs/models/src/entities/user.entity.ts
+++ libs/models/src/entities/user.entity.ts
@@ -3,14 +3,25 @@ import { Document } from 'mongoose';
 
 @Schema()
 export class User extends Document {
-  @Prop({})
-  name: string;
+  @Prop({ type: String })
+  name?: string;
 
-  @Prop({ required: true, unique: true })
-  email: string;
+  @Prop({ type: String, unique: true, sparse: true })
+  email?: string;
 
-  @Prop({ required: true, unique: true })
+  @Prop({ type: String, required: true, unique: true })
   username: string;
+
+  @Prop({
+    type: {
+      passwordHash: { type: String, required: true, select: false },
+    },
+    select: false,
+    _id: false,
+  })
+  credentials: {
+    passwordHash: string;
+  };
 }
 
 export const UserSchema = SchemaFactory.createForClass(User);
