632fd40b0ff6f8fae158471d15d1cbea1cb36384
James <happyjames19970101@gmail.com>
2025-04-09 17:46:28 +0800

    add typedoc comments for user.repository.ts.

--- libs/models/src/entities/user.entity.ts
+++ libs/models/src/entities/user.entity.ts
@@ -1,17 +1,35 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import { Document } from 'mongoose';
 
+/**
+ * @class User
+ * Represents a user entity in the system.
+ * This entity is used to store user-related information.
+ * @extends Document
+ */
 @Schema()
 export class User extends Document {
+  /**
+   * The name of the user. Users can customize the name.
+   */
   @Prop({ type: String })
   name?: string;
-
+  /**
+   * The email address of the user. The sparse option creates an index when emails exist. The unique option creates an index regardless of whether the corresponding filed exists.
+   */
   @Prop({ type: String, unique: true, sparse: true })
   email?: string;
-
+  /**
+   * The unique username of the user.
+   */
   @Prop({ type: String, required: true, unique: true })
   username: string;
-
+  /**
+   * An object containing the user's authentication credentials.<br />
+   * <ul>
+   *   <li><b>credentials.passwordHash:</b> - The hashed password of the user.</li>
+   * </ul>
+   */
   @Prop({
     type: {
       passwordHash: { type: String, required: true, select: false },
