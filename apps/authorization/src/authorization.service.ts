ecbda42cf492a54b0adadc17f43bcbaff6779bde
seconp <15290631595@163.com>
2025-04-15 22:09:01 +0800

    add logger to authorization module.

--- apps/authorization/src/authorization.service.ts
+++ apps/authorization/src/authorization.service.ts
@@ -1,8 +1,15 @@
 import { Injectable } from '@nestjs/common';
+import { Enforcer } from 'casbin';
+import { InjectPinoLogger, PinoLogger } from 'nestjs-pino';
 
 @Injectable()
 export class AuthorizationService {
-  constructor() {}
+  private enforcer: Enforcer;
+
+  constructor(
+    @InjectPinoLogger('authorization.authorization.module')
+    private readonly logger: PinoLogger,
+  ) {}
   getHello(): string {
     return 'Hello World!';
   }
