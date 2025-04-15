b62bf3e6dea771f2461bc71193851e6b6dd8f4de
James <happyjames19970101@gmail.com>
2025-04-15 17:41:35 +0800

    modify authorization.module.ts. import DatabaseModule.

--- apps/authorization/src/authorization.module.ts
+++ apps/authorization/src/authorization.module.ts
@@ -3,9 +3,10 @@ import { ModelsModule } from '@ocean.chat/models';
 
 import { AuthorizationController } from './authorization.controller';
 import { AuthorizationService } from './authorization.service';
+import { DatabaseModule } from './database/database.module';
 
 @Module({
-  imports: [ModelsModule],
+  imports: [DatabaseModule, ModelsModule],
   controllers: [AuthorizationController],
   providers: [AuthorizationService],
 })
