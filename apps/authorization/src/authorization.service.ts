29fab33b81424c9f8a8ddfd4855884e645b69f3d
seconp <15290631595@163.com>
2025-04-15 23:40:13 +0800

    In the process of implementing casbin application

--- apps/authorization/src/authorization.service.ts
+++ apps/authorization/src/authorization.service.ts
@@ -1,15 +1,29 @@
-import { Injectable } from '@nestjs/common';
-import { Enforcer } from 'casbin';
+import { Injectable, OnModuleInit } from '@nestjs/common';
+import { Enforcer, newEnforcer } from 'casbin';
 import { InjectPinoLogger, PinoLogger } from 'nestjs-pino';
+import { join } from 'path';
 
 @Injectable()
-export class AuthorizationService {
+export class AuthorizationService implements OnModuleInit {
   private enforcer: Enforcer;
 
   constructor(
     @InjectPinoLogger('authorization.authorization.module')
     private readonly logger: PinoLogger,
   ) {}
+
+  async onModuleInit() {
+    const modelPath = join(__dirname, '/config/model.conf'); // Adjust path relative to dist/src
+    this.logger.info(`Using Casbin model from: ${modelPath}`);
+    this.enforcer = await newEnforcer(modelPath);
+    // // TODO: find policies from mongodb
+    // // demo: https://casbin.org/docs/management-api/#addpolicies
+    // const permissions = [['admin', 'menu', '/admin', 'view']];
+    // await this.enforcer.addPolicies(permissions);
+    // await this.enforcer.addGroupingPolicies([['james', 'admin']]);
+    // this.logger.info('Casbin policies loaded');
+  }
+
   getHello(): string {
     return 'Hello World!';
   }
