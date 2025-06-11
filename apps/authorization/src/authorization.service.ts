167344a6421d12c5d984caf093331c44dddfb665
James <happyjames19970101@gmail.com>
2025-06-11 22:56:44 +0800

    TODO: write an adapter.  Use this adapter manage policy.

--- apps/authorization/src/authorization.service.ts
+++ apps/authorization/src/authorization.service.ts
@@ -1,5 +1,7 @@
 import { Injectable, OnModuleInit } from '@nestjs/common';
+import { InjectRedis } from '@nestjs-modules/ioredis';
 import { Enforcer, newEnforcer } from 'casbin';
+import Redis from 'ioredis';
 import { InjectPinoLogger, PinoLogger } from 'nestjs-pino';
 import { join } from 'path';
 
@@ -10,18 +12,15 @@ export class AuthorizationService implements OnModuleInit {
   constructor(
     @InjectPinoLogger('authorization.authorization.module')
     private readonly logger: PinoLogger,
+    @InjectRedis() private readonly redis: Redis,
   ) {}
 
   async onModuleInit() {
     const modelPath = join(__dirname, '/authorization_assets/model.conf'); // Adjust path relative to dist/
     this.logger.info(`Using Casbin model from: ${modelPath}`);
-    this.enforcer = await newEnforcer(modelPath);
-    // // TODO: find policies from mongodb
-    // // demo: https://casbin.org/docs/management-api/#addpolicies
-    // const permissions = [['admin', 'menu', '/admin', 'view']];
-    // await this.enforcer.addPolicies(permissions);
-    // await this.enforcer.addGroupingPolicies([['james', 'admin']]);
-    // this.logger.info('Casbin policies loaded');
+    // TODO: write an adapter with ioredis, apply it to the enforcer
+    // try {
+    // } catch (error) {}
   }
 
   getHello(): string {
