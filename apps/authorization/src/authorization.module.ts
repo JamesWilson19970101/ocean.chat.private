b3c1229c0d5417dbf83843d037a4e5cf387dc1d2
James <happyjames19970101@gmail.com>
2025-06-11 22:53:24 +0800

    Import I18nModule, ConfigModule, RedisModule into AuthorizationModule.

--- apps/authorization/src/authorization.module.ts
+++ apps/authorization/src/authorization.module.ts
@@ -1,14 +1,27 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { RedisModule } from '@nestjs-modules/ioredis';
+import { I18nModule } from '@ocean.chat/i18n';
 import { ModelsModule } from '@ocean.chat/models';
 import { IncomingMessage, ServerResponse } from 'http';
 import { LoggerModule } from 'nestjs-pino';
 
 import { AuthorizationController } from './authorization.controller';
 import { AuthorizationService } from './authorization.service';
+import { redisConfiguration } from './config/configuration';
+import { Env } from './config/env';
+import { validationSchema } from './config/validation';
 import { DatabaseModule } from './database/database.module';
 
 @Module({
   imports: [
+    I18nModule.forRoot(),
+    ConfigModule.forRoot({
+      load: [redisConfiguration],
+      isGlobal: true,
+      validationSchema,
+      envFilePath: `.env.${process.env.NODE_ENV || Env.Development}`,
+    }),
     LoggerModule.forRoot({
       pinoHttp: {
         ...(process.env.NODE_ENV !== 'production'
@@ -36,7 +49,14 @@ import { DatabaseModule } from './database/database.module';
         },
       },
     }),
-
+    RedisModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        type: 'single',
+        url: `redis://${configService.get<string>('redis.host')}:${configService.get<number>('redis.port')}`,
+      }),
+      inject: [ConfigService],
+    }),
     DatabaseModule,
     ModelsModule,
   ],
