0729ff00d34d481cd7f8e64ebfaa2fc9f534584e
James <happyjames19970101@gmail.com>
2025-07-01 12:54:24 +0800

    export all modules and services

--- libs/models/src/index.ts
+++ libs/models/src/index.ts
@@ -2,4 +2,7 @@ export * from './base.repository';
 export * from './entities';
 export * from './interfaces';
 export * from './models.module';
+export * from './mongo/mongo.module';
+export * from './mongo/mongo.service';
+export * from './mongo/watcher.service';
 export * from './repositories';
