d306857cc4e0aabb7d75e3e805f557e4f42dc4a1
James <happyjames19970101@gmail.com>
2025-07-01 12:34:06 +0800

    init enforcer with custom IoRedisAdapter

--- apps/authorization/src/authorization.service.ts
+++ apps/authorization/src/authorization.service.ts
@@ -1,5 +1,11 @@
 import { Injectable, OnModuleInit } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { InjectRedis } from '@nestjs-modules/ioredis';
+import {
+  IConnectionOptions,
+  IoRedisAdapter,
+} from '@ocean.chat/casbin-ioredis-adapter';
+import { I18nService } from '@ocean.chat/i18n';
 import { Enforcer, newEnforcer } from 'casbin';
 import Redis from 'ioredis';
 import { InjectPinoLogger, PinoLogger } from 'nestjs-pino';
@@ -13,14 +19,26 @@ export class AuthorizationService implements OnModuleInit {
     @InjectPinoLogger('authorization.authorization.module')
     private readonly logger: PinoLogger,
     @InjectRedis() private readonly redis: Redis,
+    private readonly configService: ConfigService,
+    private readonly i18nService: I18nService,
   ) {}
 
   async onModuleInit() {
     const modelPath = join(__dirname, '/authorization_assets/model.conf'); // Adjust path relative to dist/
-    this.logger.info(`Using Casbin model from: ${modelPath}`);
-    // TODO: write an adapter with ioredis, apply it to the enforcer
-    // try {
-    // } catch (error) {}
+
+    this.logger.info(
+      this.i18nService.translate('Using_Casbin_Model', {
+        modelPath,
+      }),
+    );
+
+    const redisConnectionOptions: IConnectionOptions = {
+      host: this.configService.get<string>('redis.host') as string,
+      port: this.configService.get<number>('redis.port') as number,
+      // TODO: Add password in production
+    };
+    const adapter = await IoRedisAdapter.newAdapter(redisConnectionOptions);
+    this.enforcer = await newEnforcer(modelPath, adapter);
   }
 
   getHello(): string {
