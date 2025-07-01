3d9af1cd285e847d7358201342b028a95adfee3a
James <happyjames19970101@gmail.com>
2025-07-01 12:28:52 +0800

    delete unused translations

--- libs/i18n/src/locales/en.ts
+++
@@ -1,8 +0,0 @@
-export const en = {
-  translation: {
-    HELLO: 'Hello!',
-    GOODBYE: 'Goodbye!',
-    WELCOME: 'Welcome!',
-    CURRENT_ENV: 'current environment is : {{environment}}',
-  },
-};
