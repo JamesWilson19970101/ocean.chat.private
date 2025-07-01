3de82c057e5edfbaf162eb81c98024ca58b58c44
James <happyjames19970101@gmail.com>
2025-07-01 12:40:38 +0800

    add translations

--- libs/i18n/src/translate.ts
+++ libs/i18n/src/translate.ts
@@ -5,6 +5,9 @@ export default {
       GOODBYE: 'Goodbye!',
       WELCOME: 'Welcome!',
       Current_Environment: 'Current environment is {{env}}.',
+      Watching_Collection:
+        '[Mongoose-NativeDB] Setting up single watch stream for collections: {{collections}}',
+      Using_Casbin_Model: 'Using Casbin model from {{modelPath}}',
     },
   },
   zh: {
@@ -13,6 +16,9 @@ export default {
       GOODBYE: '再见！',
       WELCOME: '欢迎！',
       Current_Environment: '当前环境是 {{env}}。',
+      Watching_Collection:
+        '[Mongoose-NativeDB] 为集合设置 watch stream: {{collections}}',
+      Using_Casbin_Model: '使用的 Casbin 模型来自 {{modelPath}}',
     },
   },
 };
