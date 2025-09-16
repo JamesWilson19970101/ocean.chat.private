13cba32189688a5df0278fc6ab4edae6673e8833
James <happyjames19970101@gmail.com>
2025-09-16 21:42:25 +0800

    feat(settings): Integrate Redis for settings management

--- libs/settings/src/settings.module.ts
+++ libs/settings/src/settings.module.ts
@@ -1,8 +1,20 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { RedisModule } from '@nestjs-modules/ioredis';
 
 import { SettingsService } from './settings.service';
 
 @Module({
+  imports: [
+    RedisModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        type: 'single', // TODO: make this configurable for cluster in production.
+        url: `redis://${configService.get<string>('redis.host')}:${configService.get<number>('redis.port')}`,
+      }),
+      inject: [ConfigService],
+    }),
+  ],
   providers: [SettingsService],
   exports: [SettingsService],
 })
