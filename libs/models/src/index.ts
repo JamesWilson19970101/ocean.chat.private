1307cdcca1d80a9008dfa37038bbcd113fac3ca2
James <happyjames19970101@gmail.com>
2025-09-17 21:40:50 +0800

    refactor(models): simplify exports using nested barrel file. Consolidate mongo-related exports into a dedicated barrel file (`mongo/index.ts`) to simplify the main library entry point. This improves code organization and maintainability.

--- libs/models/src/index.ts
+++ libs/models/src/index.ts
@@ -2,7 +2,5 @@ export * from './base.repository';
 export * from './entities';
 export * from './interfaces';
 export * from './models.module';
-export * from './mongo/mongo.module';
-export * from './mongo/mongo.service';
-export * from './mongo/watcher.service';
+export * from './mongo';
 export * from './repositories';
