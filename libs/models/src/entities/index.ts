abc4c5921a68029f2f9ca03aa1f411154b32b21c
James <happyjames19970101@gmail.com>
2025-09-30 08:01:09 +0800

    feat(models): Integrate core data models for chat.

--- libs/models/src/entities/index.ts
+++ libs/models/src/entities/index.ts
@@ -1,2 +1,6 @@
+export * from './message.entity';
+export * from './permission.entity';
+export * from './role.entity';
+export * from './room.entity';
 export * from './setting.entity';
 export * from './user.entity';
--- libs/models/src/models.module.ts
+++ libs/models/src/models.module.ts
@@ -1,12 +1,29 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 
-import { Setting, SettingSchema, User, UserSchema } from './entities';
+import {
+  Message,
+  MessageSchema,
+  Permission,
+  PermissionSchema,
+  Role,
+  RoleSchema,
+  Room,
+  RoomSchema,
+  Setting,
+  SettingSchema,
+  User,
+  UserSchema,
+} from './entities';
 import { SettingsRepository, UserRepository } from './repositories';
 
 const models = MongooseModule.forFeature([
   { name: User.name, schema: UserSchema },
   { name: Setting.name, schema: SettingSchema },
+  { name: Role.name, schema: RoleSchema },
+  { name: Permission.name, schema: PermissionSchema },
+  { name: Room.name, schema: RoomSchema },
+  { name: Message.name, schema: MessageSchema },
 ]);
 
 const repositories = [UserRepository, SettingsRepository];
