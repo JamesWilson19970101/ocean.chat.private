8b2f03bd2776bc4f8e3aa36b1d6311518021c9be
James <happyjames19970101@gmail.com>
2025-10-04 01:47:05 +0800

    feat: configure NATS client for auth microservice

--- apps/oceanchat-api-gateway/src/oceanchat-api-gateway.module.ts
+++ apps/oceanchat-api-gateway/src/oceanchat-api-gateway.module.ts
@@ -1,9 +1,21 @@
 import { Module } from '@nestjs/common';
+import { ClientsModule, Transport } from '@nestjs/microservices';
+
 import { OceanchatApiGatewayController } from './oceanchat-api-gateway.controller';
 import { OceanchatApiGatewayService } from './oceanchat-api-gateway.service';
 
 @Module({
-  imports: [],
+  imports: [
+    ClientsModule.register([
+      {
+        name: 'AUTH_SERVICE',
+        transport: Transport.NATS,
+        options: {
+          servers: ['nats://localhost:4222'],
+        },
+      },
+    ]),
+  ],
   controllers: [OceanchatApiGatewayController],
   providers: [OceanchatApiGatewayService],
 })
