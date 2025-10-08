0bc014314afed47ebd9085ab4eac344bdcb905fe
James <happyjames19970101@gmail.com>
2025-10-08 16:22:08 +0800

    test: add unit tests for local and jwt strategies

---
+++ apps/oceanchat-auth/test/strategies/jwt.strategy.spec.ts
@@ -0,0 +1,41 @@
+import { ConfigService } from '@nestjs/config';
+import { Test, TestingModule } from '@nestjs/testing';
+
+import { JwtStrategy } from '../../src/strategies/jwt.strategy';
+
+describe('Test JwtStrategy', () => {
+  let strategy: JwtStrategy;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [
+        JwtStrategy,
+        {
+          provide: ConfigService,
+          useValue: {
+            get: jest.fn((key: string) => {
+              if (key === 'JWT_SECRET') {
+                return 'secret';
+              }
+              return null;
+            }),
+          },
+        },
+      ],
+    }).compile();
+
+    strategy = module.get<JwtStrategy>(JwtStrategy);
+  });
+
+  it('should be defined', () => {
+    expect(strategy).toBeDefined();
+  });
+
+  describe('validate JWT', () => {
+    it('should return user data from payload', async () => {
+      const payload = { sub: 'userId123', username: 'testuser' };
+      const result = await strategy.validate(payload);
+      expect(result).toEqual({ _id: 'userId123', username: 'testuser' });
+    });
+  });
+});
