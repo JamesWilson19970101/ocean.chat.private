8f34c435580e42a6069654b4f381a89e58e02165
happyjames <happyjames19970101@gmail.com>
2025-10-09 14:32:44 +0000

    feat: Enable users controller

--- apps/oceanchat-auth/src/users/users.module.ts
+++ apps/oceanchat-auth/src/users/users.module.ts
@@ -3,12 +3,12 @@ import { ModelsModule } from '@ocean.chat/models';
 import { SettingsModule } from '@ocean.chat/settings';
 
 import { PasswordService } from './password.service';
-// import { UsersController } from './users.controller';
+import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
 
 @Module({
   imports: [ModelsModule, SettingsModule],
-  // controllers: [UsersController],
+  controllers: [UsersController],
   providers: [UsersService, PasswordService],
   exports: [UsersService, PasswordService],
 })
