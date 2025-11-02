81c7fd5103cd2a06c90ead2a9c9c9cdf1a324839
James <happyjames19970101@gmail.com>
2025-11-02 21:15:31 +0800

    refactor: :recycle: Simplify password validation logic in UsersService
    The `LocalStrategy`'s `validate` method was previously responsible for fetching the user, checking for their existence, and then calling another service method to verify the password. However, now only password verification is required.

--- apps/oceanchat-auth/src/strategies/local.strategy.ts
+++ apps/oceanchat-auth/src/strategies/local.strategy.ts
@@ -2,8 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
 import { BaseRpcException, ErrorCodes } from '@ocean.chat/common-exceptions';
 import { I18nService } from '@ocean.chat/i18n';
-import { AuthProvider, User } from '@ocean.chat/models';
-import { Types } from 'mongoose';
+import { User } from '@ocean.chat/models';
 import { Strategy } from 'passport-local';
 
 import { UsersService } from '../users/users.service';
@@ -18,32 +17,16 @@ export class LocalStrategy extends PassportStrategy(Strategy) {
   }
 
   async validate(username: string, password: string): Promise<Partial<User>> {
-    const userWithPassword =
-      await this.usersService.findOneByUsernameAndProvider(
-        username,
-        AuthProvider.LOCAL,
-      );
+    const user = await this.usersService.validatePassword(username, password);
 
-    if (!userWithPassword) {
-      throw new BaseRpcException(
-        this.i18nService.translate('User_not_found'),
-        ErrorCodes.USER_NOT_FOUND,
-      );
-    }
-
-    const isPasswordValid = await this.usersService.verifyPassword(
-      password,
-      userWithPassword.providers[0].passwordHash!, // Non-null assertion because I know it exists, when code reaches here, it must exist
-    );
-
-    if (!isPasswordValid) {
+    if (!user) {
       throw new BaseRpcException(
         this.i18nService.translate('INVALID_CREDENTIALS'),
         ErrorCodes.INVALID_CREDENTIALS,
       );
     }
 
-    const _id: Types.ObjectId = userWithPassword._id as Types.ObjectId;
-    return { username, _id: _id.toString() };
+    // The user object returned from validatePassword is already a partial.
+    return user;
   }
 }
