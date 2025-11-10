e4545fdc67040e1d13bcb8fa4cce36cd5ebbe60b
James <happyjames19970101@gmail.com>
2025-11-10 17:15:39 +0800

    style: :art: Format imports in app.e2e-spec.ts

--- apps/oceanchat-api-gateway/test/app.e2e-spec.ts
+++ apps/oceanchat-api-gateway/test/app.e2e-spec.ts
@@ -1,6 +1,7 @@
-import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
+import { Test, TestingModule } from '@nestjs/testing';
 import * as request from 'supertest';
+
 import { OceanchatApiGatewayModule } from './../src/oceanchat-api-gateway.module';
 
 describe('OceanchatApiGatewayController (e2e)', () => {
