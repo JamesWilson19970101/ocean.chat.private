3662949e6095299e1da9ecb8d7a6658fbdd8eda4
James <happyjames19970101@gmail.com>
2025-11-21 17:50:48 +0800

    refactor: ♻️ globalize module and update exported services

--- libs/authorization/src/index.ts
+++ libs/authorization/src/index.ts
@@ -1,2 +1,5 @@
 export * from './authorization.module';
 export * from './authorization.service';
+export * from './constants/permission-ids';
+export * from './decorators/require-permission.decorator';
+export * from './guards/permission.guard';
