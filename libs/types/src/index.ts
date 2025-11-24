1df45c11ac15b63e82b5e81b21837766e6112c3d
James <happyjames19970101@gmail.com>
2025-11-24 15:45:33 +0800

    refactor: :recycle: Move the IScopeDataProvider interface to libs/types

--- libs/types/src/index.ts
+++ libs/types/src/index.ts
@@ -1,4 +1,6 @@
 export * from './auth.type';
 export * from './authorization/permission.type';
 export * from './authorization/role.type';
+export * from './authorization/room.type';
+export * from './authorization/scope-provider.interface';
 export * from './idempotency.types';
